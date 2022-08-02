6087efa228dae9c41c62c8a100ef643357da2390
furkanogutcu <furkan.ogutcu@bil.omu.edu.tr>
2022-08-02 20:30:47 +0300

    fix: Fix dbConnection loader

--- src/loaders/dbConnection.js
+++ src/loaders/dbConnection.js
@@ -1,12 +1,21 @@
-const mongoose = require("mongoose");
+const Mongoose = require("mongoose");
+const db = Mongoose.connection;
+
+db.once("open", () => {
+    console.log("Connected to MongoDB");
+});
+
+db.once("disconnected", () => {
+    console.log("Disconnected from MongoDB");
+});
+
 
 const connectDb = async () => {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await Mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         serverSelectionTimeoutMS: 5000,
     });
-    console.log("Connected Db");
 };
 
 module.exports = connectDb;
