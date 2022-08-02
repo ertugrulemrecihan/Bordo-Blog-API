8a21ab81df1bece7eb67abd04c716a398cb99f1e
ertugrulchn <cihanertugrulemre368@gmail.com>
2022-08-02 15:51:28 +0300

    add:Add middlawares

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const config = require("./config");
 const loaders = require("./loaders");
+const successHandler = require("./api/v1/middlewares/successHandler");
+const errorHandler = require("./api/v1/middlewares/errorHandler");
 
 config();
 loaders();
 
 const app = express();
+
+app.use(successHandler);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
