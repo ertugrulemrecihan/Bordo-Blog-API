d02dc49cb871413f48da764173d0360c2f4b2722
furkanogutcu <furkan.ogutcu@bil.omu.edu.tr>
2022-08-03 10:39:35 +0300

    fix: Fix errorHandler middleware

--- src/api/v1/middlewares/errorHandler.js
+++ src/api/v1/middlewares/errorHandler.js
@@ -4,22 +4,21 @@ const ApiError = require("../responses/error/apiError");
 // eslint-disable-next-line no-unused-vars
 const ErrorHandler = (err, req, res, next) => {
     if (err instanceof ApiError) {
-        return res.status(err.statusCode).json({
+        const apiError = {
             error: {
-                message: err.message,
-                statusCode: err.statusCode,
+                message: err.message || "Something went wrong",
             },
             success: false,
-        });
-    } else {
-        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-            error: {
-                message: "Internal Server Error",
-                statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-            },
-            success: false,
-        });
+        };
+        return res.status(err.statusCode || httpStatus.INTERNAL_SERVER_ERROR).json(apiError);
     }
+
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+        error: {
+            message: "Internal Server Error",
+        },
+        success: false,
+    });
 };
 
 module.exports = ErrorHandler;
