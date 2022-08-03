9c61546a6ec0cd79e0f86de7d43df6d78bd3b6cd
furkanogutcu <furkan.ogutcu@bil.omu.edu.tr>
2022-08-03 14:42:39 +0300

    fix: Add ref name in PasswordReset model

--- src/api/v1/models/PasswordReset.js
+++ src/api/v1/models/PasswordReset.js
@@ -4,6 +4,7 @@ const PasswordResetSchema = mongoose.Schema(
     {
         user_id: {
             type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
             required: true,
         },
         token: {
