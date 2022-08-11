6c4751fdacb9567f38853649be8c3d419d4b1bbc
ertugrulchn <cihanertugrulemre368@gmail.com>
2022-08-11 14:02:56 +0300

    fix: Fix tagRoute auth

--- src/api/v1/routes/tagRoutes.js
+++ src/api/v1/routes/tagRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/TagController');
-const authenticate = require('../middlewares/authenticate');
 const paramIdValidator = require('../middlewares/paramsIdValidator');
 
-router.route('/getAll').get(authenticate, controller.fetchAll);
-router.route('/get/:id').get(authenticate, paramIdValidator, controller.fetchOneByParamsId);
+router.route('/getAll').get(controller.fetchAll);
+router.route('/get/:id').get(paramIdValidator, controller.fetchOneByParamsId);
 
 module.exports = router;
