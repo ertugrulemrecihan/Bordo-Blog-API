36099408b4398c51e92c1d94b70830259714543d
ertugrulchn <cihanertugrulemre368@gmail.com>
2022-08-11 15:06:09 +0300

    fix: Fix post authorization

--- src/api/v1/routes/postRoutes.js
+++ src/api/v1/routes/postRoutes.js
@@ -2,25 +2,24 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/PostController');
 const authenticate = require('../middlewares/authenticate');
-const authorization = require('../middlewares/authorization');
 const bodyValidator = require('../middlewares/bodyValidator');
 const paramIdValidator = require('../middlewares/paramsIdValidator');
 const schemas = require('../validations/post');
 
 // TODO: Kullanıcının aboneliği varsa getir
-router.route('/getAll/my').get(authenticate, authorization('Writer'), controller.fetchAllMyPosts);    // ! FIXME: Abone olmalı
-router.route('/get/my/:id').get(authenticate, authorization('Writer'), controller.fetchOneMyPost);    // ! FIXME: Abone olmalı
+router.route('/getAll/my').get(authenticate, controller.fetchAllMyPosts);    // ! FIXME: Abone olmalı
+router.route('/get/my/:id').get(authenticate, controller.fetchOneMyPost);    // ! FIXME: Abone olmalı
 router.route('/getAll/previews').get(controller.fetchAllPreviews);
 router.route('/get/preview/:id').get(paramIdValidator, controller.fetchOnePreview);
-router.route('/create').put(authenticate, authorization('Writer'), bodyValidator(schemas.createValidation), controller.create);
-router.route('/delete/my/:id').delete(authenticate, authorization('Writer'), paramIdValidator, controller.deleteMyPost);
-router.route('/update/my/:id').patch(authenticate, authorization('Writer'), paramIdValidator, bodyValidator(schemas.updateValidation), controller.updateMyPost);
+router.route('/create').put(authenticate, bodyValidator(schemas.createValidation), controller.create);
+router.route('/delete/my/:id').delete(authenticate, paramIdValidator, controller.deleteMyPost);
+router.route('/update/my/:id').patch(authenticate, paramIdValidator, bodyValidator(schemas.updateValidation), controller.updateMyPost);
 router.route('/addView/:id').post(authenticate, paramIdValidator, controller.addView);  // ! FIXME: Abone olmalı 
 router.route('/changeLikeStatus/:id').post(authenticate, paramIdValidator, controller.changeLikeStatus); // ! FIXME: Abone olmalı
 router.route('/addComment/:id').post(authenticate, paramIdValidator, bodyValidator(schemas.addComment), controller.addComment);
 router.route('/deleteComment/:id').post(authenticate, paramIdValidator, bodyValidator(schemas.deleteComment), controller.deleteComment);
 // TODO: Comment silme ekle
-router.route('/addTag/:id').post(authenticate, authorization('Writer'), paramIdValidator, bodyValidator(schemas.addTag), controller.addTag);
-router.route('/removeTag/:id').post(authenticate, authorization('Writer'), paramIdValidator, bodyValidator(schemas.removeTag), controller.removeTag);
+router.route('/addTag/:id').post(authenticate, paramIdValidator, bodyValidator(schemas.addTag), controller.addTag);
+router.route('/removeTag/:id').post(authenticate, paramIdValidator, bodyValidator(schemas.removeTag), controller.removeTag);
 
 module.exports = router;
