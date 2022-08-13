b86abd2f1fa4813bebbf7db754769f8c02bb0002
ertugrulchn <cihanertugrulemre368@gmail.com>
2022-08-13 21:04:50 +0300

    fix: Fix postService populate method

--- src/api/v1/services/PostService.js
+++ src/api/v1/services/PostService.js
@@ -3,25 +3,34 @@ const BaseService = require('./BaseService');
 
 class PostService extends BaseService {
     constructor() {
-        super(Post, [{
-            path: 'comments'
+        const populate = [{
+            path: 'comments',
+            populate: {
+                path: 'user_id',
+                select: 'first_name last_name email'
+            }
         }, {
             path: 'likes',
             select: 'first_name last_name email'
         }, {
             path: 'tags'
         }, {
+            path: 'viewers',
+            select: 'first_name last_name email'
+        }, {
             path: 'writer',
             select: 'first_name last_name email'
-        }]);
+        }];
+        super(Post, populate);
+        this.populate = populate;
     }
 
     fetchAllBySelect(select) {
-        return Post.find().select(select);
+        return Post.find().select(select).populate(this.populate);
     }
 
     fetchOneBySelect(query, select) {
-        return Post.findOne(query).select(select);
+        return Post.findOne(query).select(select).populate(this.populate);
     }
 }
 
