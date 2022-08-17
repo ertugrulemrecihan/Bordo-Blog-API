2f88fc3f9cfc53e58e6f722fd86db7f53a27a193
furkanogutcu <furkannogutcu@gmail.com>
2022-08-17 21:26:38 +0300

    feat: Add most used tags route in tags routes

--- src/api/v1/controllers/TagController.js
+++ src/api/v1/controllers/TagController.js
@@ -1,19 +1,19 @@
 const httpStatus = require('http-status');
-const TagService = require('../services/TagService');
+const tagService = require('../services/TagService');
 const BaseController = require('./BaseController');
 const ApiError = require('../responses/error/apiError');
 const ApiDataSuccess = require('../responses/success/apiDataSuccess');
 
 class TagController extends BaseController {
     constructor() {
-        super(TagService);
+        super(tagService);
     }
 
     // Override
     create = async (req, res, next) => {
         req.body.post_count = 0;
         try {
-            const response = await TagService.create(req.body);
+            const response = await tagService.create(req.body);
 
             if (!response) {
                 return next(
@@ -45,6 +45,40 @@ class TagController extends BaseController {
             );
         }
     };
+
+    async fetchAllMostUsedTags(req, res, next) {
+        const count = parseInt(req.params.count);
+        if (!count || count < 0) {
+            return next(
+                new ApiError(
+                    'You must send an integer greater than 0 as a parameter',
+                    httpStatus.BAD_REQUEST
+                )
+            );
+        }
+
+        const tags = await tagService.fetchAll({}, '-post_count');
+        const totalUsageCount = tags.reduce(
+            (partialSum, tag) => partialSum + tag.post_count,
+            0
+        );
+
+        const tagsWithPercentile = tags.slice(0, count).map((tag) => ({
+            tag: tag,
+            percentile:
+                totalUsageCount > 0
+                    ? (100 * tag.post_count) / totalUsageCount
+                    : 0,
+        }));
+
+        new ApiDataSuccess(
+            tagsWithPercentile,
+            'Tags fetched successfully',
+            httpStatus.OK
+        ).place(res);
+
+        return next();
+    }
 }
 
 module.exports = new TagController();
