bca7a0c613f1dbcfdd019a0924fbcda3bc4d62a4
furkanogutcu <furkannogutcu@gmail.com>
2022-09-05 17:58:56 +0300

    fix: Change plan route names

--- src/api/v1/routes/plan.routes.js
+++ src/api/v1/routes/plan.routes.js
@@ -5,10 +5,10 @@ const cache = require('../middlewares/cache.middleware');
 // eslint-disable-next-line max-len
 const paramIdValidator = require('../middlewares/params-id-validator.middleware');
 
-router.route('/get-all').get(cache(controller), controller.fetchAllByQuery);
+router.route('/').get(cache(controller), controller.fetchAllByQuery);
 
 router
-    .route('/get/:id')
+    .route('/:id')
     .get(paramIdValidator, cache(controller), controller.fetchOneByParamsId);
 
 module.exports = router;
