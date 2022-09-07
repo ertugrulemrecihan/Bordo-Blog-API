659575d68c0576c7f594938c6bac4153bf18435c
furkanogutcu <furkannogutcu@gmail.com>
2022-09-07 11:42:15 +0300

    chore: Add swagger config

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 require('express-async-errors');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const path = require('path');
+const fileUploader = require('express-fileupload');
 const configs = require('./configs');
 const loaders = require('./loaders');
 const events = require('./api/v1/scripts/events');
 const errorHandler = require('./api/v1/middlewares/error-handler.middleware');
 const routes = require('./api/v1/routes');
-const swaggerUi = require('swagger-ui-express');
+const swaggerOptions = require('./configs/swagger.config');
+
 const swaggerDocument = require('../docs/static/openapi.json');
-const fileUploader = require('express-fileupload');
-const path = require('path');
 
 configs();
 loaders();
@@ -26,7 +28,11 @@ app.use(
 
 app.use(fileUploader());
 app.use('/public', express.static(path.join(__dirname, '..', 'public')));
-app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+    '/api/v1/docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, swaggerOptions)
+);
 
 app.use('/api/v1/users', routes.user);
 app.use('/api/v1/posts', routes.post);
---
+++ src/configs/swagger.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable max-len */
+module.exports = {
+    customCss:
+        '.swagger-ui .topbar {background: #610000;} .wrapper .topbar-wrapper { display: flex; align-items: center; justify-content: center;} .link img {content: url("/public/assets/logo.svg")}',
+    customSiteTitle: 'Bordo-Blog API',
+    customfavIcon: '/public/assets/square-logo.svg',
+};
